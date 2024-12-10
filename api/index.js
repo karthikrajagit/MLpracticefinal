bfd07fd7eac29ec0aa0462be6fbd4bf7cd3c81c9
KarthikRaja0007 <karthik.raja.007c@gmail.com>
2024-12-10 20:21:43 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const startServer = async () => {
       console.log("Server is running on port 3000");
     });
 
-    app.use(express.static(path.join(__dirname, 'client/dist/')));
+    app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, 'client/dist/index.html'));
