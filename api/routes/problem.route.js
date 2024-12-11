cf4d19a52180c2a211af729328f2204eb7fb20e0
KarthikRaja0007 <karthik.raja.007c@gmail.com>
2024-12-11 14:13:44 +0530

    Initial commit

--- api/routes/problem.route.js
+++ api/routes/problem.route.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import { uploadMultipleFiles } from '../middleware/uploadmiddleware.js'; 
-import { getAllproblems,  getProblemById, upload } from '../controller/problem.controller.js';
+    import express from 'express';
+    import { uploadMultipleFiles } from '../middleware/uploadmiddleware.js'; 
+    import { getAllproblems,  getProblemById, upload } from '../controller/problem.controller.js';
 
-const router = express.Router();
-
-
-router.post('/upload', uploadMultipleFiles, upload);
-router.get('/problems', getAllproblems);
-router.get('/problems/:id', getProblemById);
+    const router = express.Router();
 
 
-export default router;
+    router.post('/upload', uploadMultipleFiles, upload);
+    router.get('/problems', getAllproblems);
+    router.get('/problems/:id', getProblemById);
+
+
+    export default router;
