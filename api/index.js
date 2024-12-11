ea89ae20d1fb25c8b81f38cbfb45a9eb6ec4ea15
KarthikRaja0007 <karthik.raja.007c@gmail.com>
2024-12-11 10:02:47 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -27,14 +27,11 @@ const startServer = async () => {
     app.use('/api/v1/admin', adminroutes);
     app.use('/api/v1/user', userroutes);
 
-    app.use(express.static(path.join(__dirname, 'client', 'dist')));
-
+    app.use(express.static('client/dist'));
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
     });
 
-
-    
 
   } catch (err) {
     console.error(err);
